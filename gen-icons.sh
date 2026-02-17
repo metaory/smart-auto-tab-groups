ab1b3ee2577aa741cd46e026b02b1e7b0062caa8
metaory <metaory@gmail.com>
2026-02-17 22:39:24 +1100

    consolide icons

--- gen-icons.sh
+++ gen-icons.sh
@@ -2,5 +2,5 @@
 set -e
 cd "$(dirname "$0")"
 for s in 16 32 48 128; do
-  rsvg-convert -w "$s" -h "$s" logo.svg -o "icons/icon-$s.png"
+  rsvg-convert -w "$s" -h "$s" logo.svg -o "assets/icons/icon-$s.png"
 done
